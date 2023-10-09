a36a643d33de91ccb38aa2588eeeee47d453d3aa
SnakTheTripper <145679842+SnakTheTripper@users.noreply.github.com>
2023-10-09 16:28:47 +0200

    MiddleWare refactored with classes
    FlaskHandler & OpcuaHandler classes implemented for readability and modularity. Code is looking a lot better.
    - maybe some optimization is in order. There are a lot of If statements and conditions inside cclass method loops, maybe I can somehow move that logic outside of the main loops and check the control_mode (flask / opcua) with a lower frequency (T =~ 1s) as that would suffice too. Currently checking with 500Hz (in every RTDE period)

--- MWare.py
+++ MWare.py
@@ -6,30 +6,26 @@ import config
 from asyncio.windows_events import WindowsSelectorEventLoopPolicy
 
 asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
-page_init = None
+
+# Set then check Update frequency for RTDE & OPCUA
 
 rtde_frequency = config.rtde_frequency
-rtde_period = 1 / config.rtde_frequency    # 0.002s for 500Hz
+rtde_period = 1 / config.rtde_frequency     # 0.002s for 500Hz
 
-if config.rtde_frequency > 500:
-    print("RTDE Update frequency cant be higher than 500Hz")
-    rtde_period = 0.002     # for 500Hz
+opcua_frequency = config.opcua_frequency
+opcua_period = 1 / config.opcua_frequency
+
+if rtde_frequency > 500:
+    print("RTDE Update frequency can not be higher than 500Hz")
+    rtde_period = 0.002  # for 500Hz
     rtde_frequency = 500
 
-def calculate_opcua_freq_divider():     # based on RTDE and desired OPCUA frequency
-    opcua_freq_divider = rtde_frequency / config.opcua_frequency
-    if config.opcua_frequency > rtde_frequency:
-        print(f"RTDE Update frequency: {rtde_frequency}")
-        print("OPCUA Update frequency can't be higher than RTDE frequency!")
-        opcua_freq_divider = 1
-        print(f"OPCUA Update frequency set to RTDE frequency: {rtde_frequency}Hz")
-    else:
-        print(f"RTDE Update frequency: {rtde_frequency}Hz")
-        print(f"OPCUA Update frequency: {config.opcua_frequency}Hz")
+if opcua_frequency > rtde_frequency:
+    print('OPCUA Update frequency can not be higher than RTDE Update frequency')
+    opcua_period = rtde_period
+    opcua_frequency = rtde_frequency
 
-    return opcua_freq_divider
-
-def initialize_zmq_connections():
+def initialize_zmq_connections(context):
     to_bridge_socket = context.socket(zmq.PUB)
     from_bridge_socket = context.socket(zmq.SUB)
     to_flask_socket = context.socket(zmq.PUB)
@@ -63,6 +59,7 @@ def initialize_zmq_connections():
 
     return to_opcua_socket, to_bridge_socket, to_flask_socket, from_bridge_socket, from_opcua_socket, from_flask_socket
 
+
 class UR10e:
     def __init__(self):
         self.current_joint = [0.0]*6
@@ -73,6 +70,7 @@ class UR10e:
 
         # represents last used move_type
         self.move_type = 1  # 0 = linear 1 = joint
+        self.control_mode = 'flask'
         self.joint_speed = 0.1
         self.joint_accel = 0.1
         self.linear_speed = 0.1
@@ -80,103 +78,165 @@ class UR10e:
         self.is_moving = False
         self.STOP = False
 
-        self.input_bit_0 = 0.0
-        self.input_bit_1 = 0.0
-        self.input_bit_2 = 0.0
-        self.input_bit_3 = 0.0
-        self.input_bit_4 = 0.0
-        self.input_bit_5 = 0.0
-        self.input_bit_6 = 0.0
-        self.input_bit_7 = 0.0
+        self.input_bit_0 = 0
+        self.input_bit_1 = 0
+        self.input_bit_2 = 0
+        self.input_bit_3 = 0
+        self.input_bit_4 = 0
+        self.input_bit_5 = 0
+        self.input_bit_6 = 0
+        self.input_bit_7 = 0
 
-        self.output_bit_0 = 0.0
-        self.output_bit_1 = 0.0
-        self.output_bit_2 = 0.0
-        self.output_bit_3 = 0.0
-        self.output_bit_4 = 0.0
-        self.output_bit_5 = 0.0
-        self.output_bit_6 = 0.0
-        self.output_bit_7 = 0.0
+        self.output_bit_0 = 0
+        self.output_bit_1 = 0
+        self.output_bit_2 = 0
+        self.output_bit_3 = 0
+        self.output_bit_4 = 0
+        self.output_bit_5 = 0
+        self.output_bit_6 = 0
+        self.output_bit_7 = 0
+
+        self.page_init = False
 
     def update_local_dataset(self, data_dictionary):
         for key, value in data_dictionary.items():
-            setattr(self, key, value)
+            if hasattr(self, key):
+                setattr(self, key, value)
+            else:
+                print(f'Tried updating {key}, but it not an attribute of the object!')
 
-    def gather_to_send(self):   # send all attributes
-        return self.__dict__
+    def gather_to_send_all(self):   # send all attributes
+        return {key: value for key, value in self.__dict__.items() if
+                key not in ['page_init']}
 
-local_robot_state = UR10e()
-context = zmq.asyncio.Context()
-(to_opcua_socket,
- to_bridge_socket,
- to_flask_socket,
- from_bridge_socket,
- from_opcua_socket,
- from_flask_socket) = initialize_zmq_connections()
+    def gather_to_send_no_control(self):   # send all attributes
+        return {key: value for key, value in self.__dict__.items() if
+                key not in ['target_joint',
+                            'target_tcp',
+                            'move_type',
+                            'joint_speed',
+                            'joint_accel',
+                            'linear_speed',
+                            'linear_accel',
+                            'page_init']}
 
-async def update_opcua():
-    # serializes the dictionary of robot attributes
-    serialized_message = json.dumps(local_robot_state.gather_to_send()).encode()  # dictionary of robot attributes
-    # print('sending package to opcua server')
-    await to_opcua_socket.send_multipart([b"update_package", serialized_message])
+class FlaskHandler:
+    def __init__(self, to_flask, from_flask, to_bridge, local_ur):
+        self.to_flask_sock = to_flask
+        self.to_bridge_sock = to_bridge
 
-async def send_to_flask(pub_socket, topic, message, last_moving_state):     # FW current to Flask
-    if local_robot_state.is_moving or page_init:
-        await pub_socket.send_multipart([topic, message])
-    elif last_moving_state and not local_robot_state.is_moving:
-        await pub_socket.send_multipart([topic, message])
+        self.from_flask_sock = from_flask
+        self.from_flask_sock.setsockopt(zmq.SUBSCRIBE, b"Move_Commands")
+        self.from_flask_sock.setsockopt(zmq.SUBSCRIBE, b"page_init")
 
-# local dataset AND opcua
-async def update_data(decoded_message, i, opcua_freq_divider):
-    local_robot_state.update_local_dataset(decoded_message)
-    if i >= opcua_freq_divider:
-        await update_opcua()
-        i = 1
-    else:
-        i += 1
-    return i
+        self.local_ur10 = local_ur
 
-async def receive_from_bridge(pub_socket, sub_socket):
-    sub_socket.setsockopt(zmq.SUBSCRIBE, b"Joint_States")
-    opcua_freq_divider = calculate_opcua_freq_divider()
-    global page_init    # changes to True on webpage is loaded by a client
-    last_moving_state = False
-    i = 1
+    async def send(self):
+        # send updates to Flask
+        last_drops = 10
+        while True:
+            if last_drops > 0 or self.local_ur10.page_init:
+                topic = b"Joint_States"
+                serialized_message = None
+
+                if self.local_ur10.control_mode == 'flask':
+                    serialized_message = json.dumps(self.local_ur10.gather_to_send_no_control()).encode()
+                elif self.local_ur10.control_mode == 'opcua':
+                    serialized_message = json.dumps(self.local_ur10.gather_to_send_all()).encode()
+
+                try:
+                    await self.to_flask_sock.send_multipart([topic, serialized_message])
+                except Exception as e:
+                    print(f'Can not send update to Flask: {e}')
+
+                if not self.local_ur10.is_moving:
+                    last_drops -= 1     # decrease only when not moving
+
+            if self.local_ur10.is_moving:
+                last_drops = 10
+
+            await asyncio.sleep(rtde_period)
+
+    async def receive(self):
+        # receive from Flask
+        while True:
+            if self.local_ur10.control_mode == 'flask':
+                topic, serialized_message = await self.from_flask_sock.recv_multipart()
+
+                if topic == b'Move_Commands':
+                    # quickly forward to bridge
+                    await self.to_bridge_sock.send_multipart([topic, serialized_message])
+                    # then unpack and update local dataset
+                    self.local_ur10.update_local_dataset(json.loads(serialized_message.decode()))
+                elif topic == b'page_init':
+                    self.local_ur10.page_init = True
+                    await asyncio.sleep(5 * rtde_period)
+                    self.local_ur10.page_init = False
+            elif self.local_ur10.control_mode == 'opcua':
+                await asyncio.sleep(1)  # Sleep a bit when not in flask mode
+
+class OpcuaHandler:
+    def __init__(self, to_opcua, from_opcua, to_bridge, local_ur):
+        self.to_opcua_sock = to_opcua
+        self.to_bridge_sock = to_bridge
+
+        self.from_opcua_sock = from_opcua
+        self.from_opcua_sock.setsockopt(zmq.SUBSCRIBE, b"opcua_command")
+
+        self.local_ur10 = local_ur
+
+    async def send(self):
+        # send to OPCUA from MW database
+        serialized_message = None
+        while True:
+            if self.local_ur10.control_mode == 'flask':
+                serialized_message = json.dumps(self.local_ur10.gather_to_send_all()).encode()  # dictionary of robot attributes
+            elif self.local_ur10.control_mode == 'opcua':
+                serialized_message = json.dumps(self.local_ur10.gather_to_send_no_control()).encode()
+
+            await self.to_opcua_sock.send_multipart([b"update_package", serialized_message])
+            await asyncio.sleep(opcua_period)
+
+    async def receive(self):        # receive from OPCUA
+        while True:
+            if self.local_ur10.control_mode == 'opcua':
+                [topic, serialized_message] = await self.from_opcua_sock.recv_multipart()
+                print('msg from opcua arrived to MW')
+
+                # quickly forward to bridge
+                await self.to_bridge_sock.send_multipart([topic, serialized_message])
+                # then unpack and update local dataset
+                self.local_ur10.update_local_dataset(json.loads(serialized_message.decode()))
+
+            elif self.local_ur10.control_mode == 'flask':
+                await asyncio.sleep(1)      # wait a bit if not in opcua control
+
+async def receive_from_bridge(from_bridge_sock, local_ur10):
+    from_bridge_sock.setsockopt(zmq.SUBSCRIBE, b'Joint_States')
     while True:
-        topic, message = await sub_socket.recv_multipart()
-        decoded_message = json.loads(message.decode())
+        topic, serialized_message = await from_bridge_sock.recv_multipart()
+        local_ur10.update_local_dataset(json.loads(serialized_message.decode()))    # blocking command
 
-        last_moving_state = await send_to_flask(pub_socket, topic, message, last_moving_state)
-
-        i = await update_data(decoded_message, i, opcua_freq_divider)     # updates local AND opcua (with lower freq.)
-        if last_moving_state:   # one last call to keep opcua up to date
-            await update_opcua()
-
-        await asyncio.sleep(rtde_period / 2)
-
-
-async def zmq_forward_flask_to_bridge(pub_socket, sub_socket):
-    sub_socket.setsockopt(zmq.SUBSCRIBE, b"Move_Commands")
-    sub_socket.setsockopt(zmq.SUBSCRIBE, b"page_init")
-    global page_init
-    while True:
-        [topic, message] = await sub_socket.recv_multipart()
-        if topic == b"Move_Commands":
-            await pub_socket.send_multipart([topic, message])   # forward quickly to bridge
-            print(message.decode())
-        elif topic == b"page_init":     # set page_init to True -> current data flow opens from bridge to flask
-            page_init = True
-            await asyncio.sleep(5 * rtde_period)   # might need more time to work properly
-            page_init = False
-
-        await asyncio.sleep(rtde_period / 2)
-
-
+        await asyncio.sleep(rtde_period)
 async def main():
-    await asyncio.gather(
-        receive_from_bridge(to_flask_socket, from_bridge_socket),
-        zmq_forward_flask_to_bridge(to_bridge_socket, from_flask_socket)
-    )
+    local_ur10 = UR10e()
+
+    context = zmq.asyncio.Context()
+    (to_opcua_socket,
+     to_bridge_socket,
+     to_flask_socket,
+     from_bridge_socket,
+     from_opcua_socket,
+     from_flask_socket) = initialize_zmq_connections(context)
+
+    flask_handler = FlaskHandler(to_flask_socket, from_flask_socket, to_bridge_socket, local_ur10)
+    opcua_handler = OpcuaHandler(to_opcua_socket, from_opcua_socket, to_bridge_socket, local_ur10)
+
+    await asyncio.gather(flask_handler.send(),
+                         flask_handler.receive(),
+                         opcua_handler.send(),
+                         opcua_handler.receive(),
+                         receive_from_bridge(from_bridge_socket, local_ur10))
 
 if __name__ == "__main__":
     asyncio.run(main())
